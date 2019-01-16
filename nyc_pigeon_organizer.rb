18d1a889a3ba2cf01c78ace4142ff2c04d56653b
Calie Rushton <calielrushton@gmail.com>
2019-01-16 14:47:14 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,18 @@
+require 'pry'
+
 def nyc_pigeon_organizer(data)
-  # write your code here!
+  pigeons_by_name = {}
+  name_array = []
+
+  data.each do |k, v|
+    name_array.push v.values
+    name_array.flatten.uniq!.each do |name|
+      data.each do |k, v|
+      pigeons_by_name[name] = {:color => [], :gender => [], :lives => []}
+    # get name of first, second, third keys in hash instead of hardcoding?
+    end
+  end
+end
+
+pigeons_by_name
 end
