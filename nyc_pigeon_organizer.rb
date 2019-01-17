2c99b5f8f92bc681cd3db2a200dc24023077ef20
Calie Rushton <calielrushton@gmail.com>
2019-01-17 09:18:25 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,7 +1,27 @@
 require 'pry'
 
 def nyc_pigeon_organizer(data)
-  pigeons_by_name = {}
+  pigeons = {}
 
-  pigeons_by_name
+  data.each do |attribute, details| #attribute:color/gender/lives, details: purple.subway/female
+    details.each do |option, names_array| # option: purple/subway/female
+      names_array.each do |name| # get down to name of pigeon level
+        if pigeons.has_key?(name) # is the pigeon name in the new hash?
+          if pigeons[name].has_key?(attribute) # yes, does the pigeon have these properties?
+            pigeons[name][attribute] << option.to_s # yes, push any remaining options into array
+          else
+            pigeons[name][attribute] = [option.to_s] # no, add them as value of attribute, in an array
+          end
+        else  pigeons[name]= {attribute => [option.to_s]} # no, pigeon not in hash so add it with a hash containing properties
+        end
+      end
+    end
+  end
+
+pigeons
 end
+
+# I spent too long trying to build the hash then put the elements into it.
+# Instead, ask is the pigeon in the new hash?
+# If not, add it with its properties
+# otherwise, add the properties to the existing pigeon key.
